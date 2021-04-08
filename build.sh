7f1ca94b19dc201b4ff6c58cb4c6ae2613f8c4e3
WincerChan <WincerChan@gmail.com>
2021-04-08 20:41:56 +0800

    add params

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ export GOPRIVATE="github.com/WincerChan"
 git config --global url."https://${GH_TOKEN}@github.com".insteadOf "https://github.com"
 git clone https://github.com/WincerChan/BlogContent
 hugo --minify
-go run BlogContent/encrypt.go
+go run BlogContent/encrypt.go encrypt_posts.toml
