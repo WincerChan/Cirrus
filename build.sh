71f9a9aa7115a5472ad1ed65524f36b32ed91c88
LoeRfy <WincerChan@users.noreply.github.com>
2021-04-08 23:56:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 export GOPRIVATE="github.com/WincerChan"
 git config --global url."https://${GH_TOKEN}@github.com".insteadOf "https://github.com"
+echo "Cloning Blogs..."
 git clone https://github.com/WincerChan/BlogContent
+echo "Clone Blogs Succeed. Start hugo --minify..."
 hugo --minify
+Echo "Start Encrypting pages..."
 go run BlogContent/encrypt.go encrypt_posts.toml
