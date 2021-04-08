61c7e590c1c5e89b61b109714b85d61fca8bf693
WincerChan <WincerChan@gmail.com>
2021-04-08 21:27:18 +0800

    add some echo

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 export GOPRIVATE="github.com/WincerChan"
 git config --global url."https://${GH_TOKEN}@github.com".insteadOf "https://github.com"
+echo "now cloning BLogContent"
 git clone https://github.com/WincerChan/BlogContent
+echo "Cloned succeed"
 hugo --minify
+echo "encrypted"
 go run BlogContent/encrypt.go encrypt_posts.toml
