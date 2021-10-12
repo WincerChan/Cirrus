1ef6da201dd7adb2ca873af8dc233dd9a26cd215
WincerChan <WincerChan@gmail.com>
2021-10-12 23:39:45 +0800

    remove front-end steps

--- build.sh
+++ build.sh
@@ -3,7 +3,5 @@ echo "Cloning Blogs..."
 git config --global url."https://${GH_TOKEN}@github.com".insteadOf "https://github.com"
 git clone https://github.com/WincerChan/BlogContent
 echo "Clone Blogs Succeed. Start Building..."
-yarn install
-yarn build
 echo "Start Encrypting Pages..."
 go run BlogContent/encrypt.go encrypt_posts.toml
