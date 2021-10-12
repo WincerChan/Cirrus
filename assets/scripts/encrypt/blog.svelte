a74450136b9582debda251957d516c0553494dad
WincerChan <WincerChan@gmail.com>
2021-10-12 19:00:07 +0800

    update some deps

--- assets/scripts/encrypt/blog.svelte
+++ assets/scripts/encrypt/blog.svelte
@@ -1,19 +1,21 @@
 <script>
     import { enc } from "crypto-js";
-    let password;
+    let password, input_pass;
+    $: err_msg = null;
     const ele = document.getElementsByTagName("content")[0];
     const aes_block_size = 16;
-    function appendLeft(passwd, length) {
-        pwd = "";
-        for (let i = passwd.length; i < 16; i++) pwd += "a";
-        return pwd + passwd;
+    function fillToPassword() {
+        let pwd = "";
+        for (let i = password.length; i < aes_block_size; i++) pwd += "a";
+        return pwd + password;
     }
     function decrypt() {
-        pwd = appendLeft(pwd, aes_block_size);
+        let pwd = fillToPassword();
+        console.log(pwd);
         let key = CryptoJS.enc.Utf8.parse(pwd),
             encryptedHexStr = CryptoJS.enc.Hex.parse(ele.innerHTML),
             encryptedBase64Str = CryptoJS.enc.Base64.stringify(encryptedHexStr);
-        decrypted = CryptoJS.AES.decrypt(encryptedBase64Str, key, {
+        let decrypted = CryptoJS.AES.decrypt(encryptedBase64Str, key, {
             iv: key,
             mode: CryptoJS.mode.CBC,
             padding: CryptoJS.pad.Pkcs7,
@@ -21,15 +23,15 @@
         try {
             ele.innerHTML = decrypted.toString(enc.Utf8);
             ele.classList.remove("hidden");
-            document.getElementById("input").remove();
+            input_pass.remove();
         } catch {
             console.log("Error Password");
-            document.getElementById("error_msg");
+            err_msg = true;
         }
     }
 </script>
 
-<div id="input" class="my-4">
+<div class="my-4 ml-6" bind:this={input_pass}>
     <input
         type="text"
         placeholder="🔑 Input Password, Press `Enter`"
@@ -38,7 +40,8 @@
         onkeypress="EnterPress(event)"
         class="transition ease-linear duration-300 border-hyper w-64 border-b-2 px-2 py-1 focus:border-hyper focus:outline-none"
     />
-    <span id="error_msg" class="text-red-600 pl-6 hidden"
-        >🙅Wrong Password！</span
-    >
+    <button on:click={decrypt} class="border rounded p-2">提交</button>
+    {#if err_msg}
+        <span id="error_msg" class="text-red-600 pl-6">🙅Wrong Password！</span>
+    {/if}
 </div>
