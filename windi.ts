15f854e3f64de875091b7797ef28583d0e87b743
WincerChan <WincerChan@gmail.com>
2022-03-02 23:33:18 +0800

    update windi.ts api
    rename windi.html to base.windi

--- windi.ts
+++ windi.ts
@@ -1,40 +1,76 @@
 const { Processor } = require('windicss/lib')
 const { HTMLParser, CSSParser } = require('windicss/utils/parser')
+const { StyleSheet: Ss } = require('windicss/utils/style')
 const htmlparser2 = require('htmlparser2')
 
 const fs = require('fs')
 const glob = require('glob')
 
 
-const getMatchFiles = async () => {
-    let allFiles = await glob.sync('./assets/css/*.html');
-    for (const i in allFiles) {
-        let t = fs.readFileSync(allFiles[i]).toString();
-        console.log(generateStyles(t))
+const MATCH_CLASSES_FILES = '{layouts,assets}/**/*.{html,svg,svelte}'
+
+// only allow one style file
+const MATCH_STYLES_FILE = 'assets/css/base.windi'
+
+const OUTPUT_STYLES_FILE = 'static/windi.css'
+
+const replaceWithCompiledFiles = (fileHTMLs, fileStyles) => {
+    for (const file in fileHTMLs) {
+        fs.writeFileSync(`${file}`, fileHTMLs[file])
     }
+    const outputStyle = Object.values(fileStyles).reduce((acc, curr) => acc.extend(curr), new Ss())
+        .combine()
+        .build(true)
+    fs.writeFileSync(OUTPUT_STYLES_FILE, outputStyle)
 }
-function generateStyles(html) {
-    // Get windi processor
+
+const processMatchedFiles = async () => {
+    let matchedFiles = await glob.sync(MATCH_CLASSES_FILES),
+        fileStyles = {},
+        fileHTMLs = {};
+
+    matchedFiles.forEach(file => {
+        let content = fs.readFileSync(file).toString();
+        let r = extractClasses(content)
+        fileStyles[file] = r.styles;
+        fileHTMLs[file] = r.outputHTML;
+    })
+    let styles = fs.readFileSync(MATCH_STYLES_FILE).toString()
+    fileStyles[MATCH_STYLES_FILE] = extractStyles(styles);
+    replaceWithCompiledFiles(fileHTMLs, fileStyles);
+}
+
+const extractClasses = (html,) => {
     const processor = new Processor()
-
-    console.log(htmlparser2.parseDocument(html))
-    // Parse all classes and put into one line to simplify operations
-    const htmlClasses = new HTMLParser(html)
-        .parseClasses()
-        .map(i => i.result)
-        .join(' ')
-
-    // Generate preflight based on the HTML we input
+    const parser = new HTMLParser(html)
     const preflightSheet = processor.preflight(html)
 
-    // Process the HTML classes to an interpreted style sheet
-    const interpretedSheet = processor.interpret(htmlClasses).styleSheet
+    const PREFIX = 'wir-'
+    const outputCSS = []
+    let outputHTML = ''
+    let indexStart = 0
+    parser.parseClasses().forEach((p) => {
+        outputHTML += html.substring(indexStart, p.start)
 
-    // Build styles
-    const APPEND = false
-    const MINIFY = false
-    const styles = interpretedSheet.extend(preflightSheet, APPEND).build(MINIFY)
+        const style = processor.compile(p.result, PREFIX)
+        outputCSS.push(style.styleSheet)
+        outputHTML += [style.className, ...style.ignored].join(' ')
+        indexStart = p.end
+    })
+    outputHTML += html.substring(indexStart)
 
-    return styles
+    const styles = outputCSS
+        .reduce((acc, curr) => acc.extend(curr), preflightSheet)
+    return {
+        styles,
+        outputHTML
+    }
 }
-getMatchFiles()
+
+const extractStyles = (css) => {
+    const processor = new Processor()
+    const parser = new CSSParser(css, processor)
+    return parser.parse()
+}
+
+processMatchedFiles()
