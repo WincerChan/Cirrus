ab7b0f9d1f353ca237d938c922fc45f79b3e8532
WincerChan <WincerChan@gmail.com>
2022-03-06 00:21:50 +0800

    update encrypt

--- assets/scripts/encrypt/blog.svelte
+++ assets/scripts/encrypt/blog.svelte
@@ -1,25 +1,13 @@
 <script>
-    import { enc, AES, mode, pad } from "crypto-js";
-    let password, input_pass;
-    $: err_msg = null;
+    import { enc, AES } from "crypto-js";
+    let password,
+        input_pass,
+        err_msg = false;
     const ele = document.getElementsByTagName("content")[0];
-    const aes_block_size = 16;
-    function fillToPassword() {
-        let pwd = "";
-        for (let i = password.length; i < aes_block_size; i++) pwd += "a";
-        return pwd + password;
-    }
     function decrypt() {
-        let pwd = fillToPassword();
-        console.log(pwd);
-        let key = enc.Utf8.parse(pwd),
-            encryptedHexStr = enc.Hex.parse(ele.innerHTML),
-            encryptedBase64Str = enc.Base64.stringify(encryptedHexStr);
-        let decrypted = AES.decrypt(encryptedBase64Str, key, {
-            iv: key,
-            mode: mode.CBC,
-            padding: pad.Pkcs7,
-        });
+        let key = enc.Utf8.parse(password),
+            cipher = enc.Hex.parse(ele.innerHTML);
+        let decrypted = AES.decrypt(cipher, key);
         try {
             ele.innerHTML = decrypted.toString(enc.Utf8);
             ele.classList.remove("hidden");
