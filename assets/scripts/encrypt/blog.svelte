b0f988db6361367a12ce11a760137aa2223ad2fd
WincerChan <WincerChan@gmail.com>
2022-03-06 10:30:35 +0800

    fix input password style

--- assets/scripts/encrypt/blog.svelte
+++ assets/scripts/encrypt/blog.svelte
@@ -10,7 +10,7 @@
         let decrypted = AES.decrypt(cipher, key);
         try {
             ele.innerHTML = decrypted.toString(enc.Utf8);
-            ele.classList.remove("hidden");
+            ele.classList.toggle("hidden");
             input_pass.remove();
         } catch {
             console.log("Error Password");
@@ -19,17 +19,17 @@
     }
 </script>
 
-<div class="my-4 ml-6" bind:this={input_pass}>
+<div class="mt-2 mb-10 ml-6" bind:this={input_pass}>
     <input
-        type="text"
+        type="password"
         placeholder="🔑 Input Password, Press `Enter`"
         bind:value={password}
-        class="transition ease-linear duration-300 border-hyper w-64 border-b-2 px-2 py-1 focus:border-hyper focus:outline-none"
+        class="transition ease-linear duration-300 dark:bg-transparent bg-transparent border-[#0d9488] w-56 sm:w-72 border-b-2 px-2 py-1 focus:border-hyper focus:outline-none"
     />
     <button
         on:click={decrypt}
-        class="border rounded p-2 text-base bg-hyper text-[#f1f5f9] dark:text-[#1E293B]"
-        >提交</button
+        class="rounded p-2 text-sm bg-[#0d9488] hover:opacity-80 transition ease-linear duration-300 text-[#f1f5f9] dark:text-[#1E293B]"
+        >Submit</button
     >
     {#if err_msg}
         <span id="error_msg" class="text-red-600 pl-6">🙅Wrong Password！</span>
