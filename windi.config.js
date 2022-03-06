e70c2cf4aa1ddea2bd88831583ea4e67f01a3f37
WincerChan <WincerChan@gmail.com>
2022-03-06 17:49:25 +0800

    delete windicss.config.js

--- windi.config.js
+++
@@ -1,9 +0,0 @@
-const helper = require('windicss/helpers');
-
-const production = !process.env.NODE_ENV !== 'production';
-
-
-
-module.exports = helper.defineConfig({
-  extract: { include: ["./layouts/**/*.html", "./assets/**/*.svg", "./assets/**/*.html", "./assets/**/*.svelte"] },
-})
