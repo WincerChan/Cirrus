6024b501eb13d934362de457b46130b4dd497492
WincerChan <WincerChan@gmail.com>
2022-03-07 21:05:44 +0800

    update input box animation

--- assets/scripts/encrypt/blog.svelte
+++ assets/scripts/encrypt/blog.svelte
@@ -2,6 +2,7 @@
     import { enc, AES } from "crypto-js";
     let password: string,
         show_input: boolean = true,
+        on_focus: HTMLDivElement = false,
         err_msg = false;
     const ele: HTMLDivElement = document.getElementsByTagName("content")[0];
     function decrypt() {
@@ -19,16 +20,25 @@
 </script>
 
 {#if show_input}
-    <div class="mt-2 mb-10 ml-6">
-        <input
-            type="password"
-            placeholder="🔑 Input Password"
-            bind:value={password}
-            class="transition ease-linear duration-300 dark:bg-transparent bg-transparent border-[#0d9488] w-56 sm:w-72 border-b-2 px-2 py-1 focus:outline-none"
-        />
+    <div class="mt-2 mb-10 ml-6 flex items-start">
+        <div class="w-56 sm:w-72 h-9 px-2 text-center ">
+            <input
+                type="password"
+                placeholder="🔑 Input Password"
+                bind:value={password}
+                on:focus={() => (on_focus.style.width = "100%")}
+                on:blur={() => (on_focus.style.width = "0")}
+                class="transition mb-2 ease-linear px-2 py-1 z-10 relative duration-300 dark:bg-transparent bg-transparent border-[#0d9488] border-opacity-40 w-full border-b-2 focus:outline-none"
+            />
+            <div
+                bind:this={on_focus}
+                style="transition: width .2s;"
+                class="w-0 -top-26px relative h-[2px] mx-auto self-center bg-[#0d9488] inline-block hover:w-10"
+            />
+        </div>
         <button
             on:click={decrypt}
-            class="rounded-md px-3 py-2 text-sm bg-[#0d9488] hover:opacity-80 transition ease-linear duration-300 text-[#f1f5f9] dark:text-[#1E293B]"
+            class="rounded-md px-3 py-2 text-sm bg-[#0d9488] hover:opacity-100 opacity-90 transition ease-linear duration-300 text-[#f1f5f9] dark:text-[#1E293B]"
             >Submit</button
         >
         {#if err_msg}
