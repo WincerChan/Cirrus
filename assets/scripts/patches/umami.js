74eb3d91086216e37482cd506aaa0b92f1a9bf33
WincerChan <WincerChan@gmail.com>
2022-10-27 07:47:58 +0800

    remove a tag event
    bump vendor version

--- assets/scripts/patches/umami.js
+++ assets/scripts/patches/umami.js
@@ -2,7 +2,7 @@ const registerUmamiEvents = () => {
     const eventEls = document.querySelectorAll('*[data-track]')
     eventEls.forEach(el => {
         const attr = el.getAttribute('data-track')
-        el.onclick = () => window.umami(attr)
+        el.onclick = () => window.umami('Click ' + attr)
     })
 }
 export default registerUmamiEvents;
